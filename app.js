2addb9bbc27929f0ed672510163d6bde52e2e30c
Kelvin Maues <kgmdeveloper@gmail.com>
2019-11-10 21:10:25 -0300

    Routes and handle requests manually

--- app.js
+++ app.js
@@ -1,21 +1,7 @@
-const http = require('http');
+const http = require("http");
+const routes = require("./routes");
 
-const server = http.createServer((req, res) => {
-  const { url } = req;
-  res.setHeader('Content-type', 'text/html');
-  if (url === '/') {
-    res.write('<html>');
-    res.write('<head><title>Enter Message</title></head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit"></button>Send</form></body>');
-    res.write('</html>');
-    return res.end();
-  }
-  res.write('<html>');
-  res.write('<head><title>My First Page</title></head>');
-  res.write('<body><h1>Hello from my Node.js server!</h1></body>');
-  res.write('</html>');
-  res.end();
-});
+const server = http.createServer(routes);
 
 // SERVER LISTING ON PORT 3000
 server.listen(3000);
