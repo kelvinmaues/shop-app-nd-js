cfe30ed85405358e6fc0d9b6ab53a92af77e75c6
Kelvin Gaia Maués <kelvin.gaia@matchboxbrasil.com>
2019-11-18 08:42:36 -0300

    New third library

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const http = require("http");
 const express = require("express");
 
 const app = express();
 // const routes = require("./routes");
 
-const server = http.createServer(app);
+app.use("/products", (req, res, next) => {
+  res.send("<h1>The product list!</h1>");
+});
+
+app.use((req, res, next) => {
+  res.send("<h1>Hello from Express.js!</h1>");
+});
+
 // SERVER LISTING ON PORT 3000
-server.listen(3000);
+app.listen(3000);
