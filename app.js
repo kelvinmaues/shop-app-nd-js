be6c321ad69bf36396d8f3d441fa6b9811ec1d53
Kelvin Gaia Maués <kelvin.gaia@matchboxbrasil.com>
2019-11-30 12:17:14 -0300

    New package to parse requests

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 // const routes = require("./routes");
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/products", (req, res, next) => {
   res.send("<h1>The product list!</h1>");
+  next();
+});
+
+app.use("/add-product", (req, res, next) => {
+  console.log("In another middleware!");
+  // res.send("<h1>The 'Add the product page'</h1>");
+  const form = `
+    <form action="/product" method="POST">
+      <input type="text" name="title">
+      <button type="submit" >Add product</button>
+    </form>
+  `;
+  res.send(form);
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use((req, res, next) => {
