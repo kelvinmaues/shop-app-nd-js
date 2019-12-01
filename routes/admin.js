b2ea9c072fd8ba8f065a2de53f343f172ca31238
Kelvin Gaia Maués <kelvin.gaia@matchboxbrasil.com>
2019-12-01 20:13:01 -0300

    Add new view file for page not found

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,10 @@
 const express = require("express");
+const path = require("path");
 
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  console.log("In another middleware!");
-  // res.send("<h1>The 'Add the product page'</h1>");
-  const form = `
-    <form action="/admin/product" method="POST">
-      <input type="text" name="title">
-      <button type="submit" >Add product</button>
-    </form>
-  `;
-  res.send(form);
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 router.post("/product", (req, res, next) => {
