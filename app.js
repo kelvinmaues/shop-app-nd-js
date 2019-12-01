4b1139147015ca6a7ff20830f67d27cdf4fb2f7c
Kelvin Maues <kgmdeveloper@gmail.com>
2019-12-01 19:04:31 -0300

    Routing with express and 404 page handled

--- app.js
+++ app.js
@@ -2,34 +2,24 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-// const routes = require("./routes");
+
+// parser
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/products", (req, res, next) => {
-  res.send("<h1>The product list!</h1>");
-  next();
-});
+// routes
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use("/add-product", (req, res, next) => {
-  console.log("In another middleware!");
-  // res.send("<h1>The 'Add the product page'</h1>");
-  const form = `
-    <form action="/product" method="POST">
-      <input type="text" name="title">
-      <button type="submit" >Add product</button>
-    </form>
-  `;
-  res.send(form);
-});
-
-app.use("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.send("<h1>Hello from Express.js!</h1>");
+  res.status(404).send("<h1>Page not found!</h1>");
 });
+// app.use("/products", (req, res, next) => {
+//   res.send("<h1>The product list!</h1>");
+//   next();
+// });
 
 // SERVER LISTING ON PORT 3000
 app.listen(3000);
---
+++ routes/admin.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/add-product", (req, res, next) => {
+  console.log("In another middleware!");
+  // res.send("<h1>The 'Add the product page'</h1>");
+  const form = `
+    <form action="/product" method="POST">
+      <input type="text" name="title">
+      <button type="submit" >Add product</button>
+    </form>
+  `;
+  res.send(form);
+});
+
+router.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+module.exports = router;
