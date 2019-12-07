902c63a3bb786926e62565680815cd5fc2f87259
Kelvin Maues <kgmdeveloper@gmail.com>
2019-12-07 11:19:56 -0300

    Use internal data

--- routes/shop.js
+++ routes/shop.js
@@ -4,8 +4,10 @@ const path = require("path");
 const router = express.Router();
 
 const rootDir = require("../util/path");
+const admin = require("./admin");
 
 router.get("/", (req, res, next) => {
+  console.log(admin.products);
   res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
