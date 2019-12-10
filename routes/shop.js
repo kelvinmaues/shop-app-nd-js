89fb92d48d2e95fecb2f229a5edea89d9ad2a1bf
Kelvin Gaia Maués <kelvin.gaia@matchboxbrasil.com>
2019-12-09 21:28:27 -0300

    Using layout main template from pug to the pages

--- routes/shop.js
+++ routes/shop.js
@@ -3,14 +3,14 @@ const path = require("path");
 
 const router = express.Router();
 
-const rootDir = require("../util/path");
+// const rootDir = require("../util/path");
 const admin = require("./admin");
 
 router.get("/", (req, res, next) => {
   const { products } = admin;
   console.log(admin.products);
   // res.sendFile(path.join(rootDir, "views", "shop.html"));
-  res.render("shop", { products, docTitle: "My Shop" });
+  res.render("shop", { products, pageTitle: "My Shop", path: "/" });
 });
 
 module.exports = router;
