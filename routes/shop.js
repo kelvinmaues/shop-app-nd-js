18bd31804edd2f8eccbcfcc23a83479c6e0e9245
Kelvin Gaia Maués <kelvin.gaia@matchboxbrasil.com>
2019-12-30 11:31:49 -0300

    Finish templates with handlebars files

--- routes/shop.js
+++ routes/shop.js
@@ -1,20 +1,21 @@
-const express = require("express");
-const path = require("path");
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
-// const rootDir = require("../util/path");
-const admin = require("./admin");
-
-router.get("/", (req, res, next) => {
-  const { products } = admin;
-  console.log(admin.products);
-  // res.sendFile(path.join(rootDir, "views", "shop.html"));
-  res.render("shop", {
-    products,
-    pageTitle: "My Shop",
-    path: "/",
-    hasProducts: products.length > 0
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
   });
 });
 
