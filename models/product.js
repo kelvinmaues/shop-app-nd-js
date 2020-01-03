96fee819f8ffc052b864cc1abfb4b658d067d0dd
Kelvin Gaia Maués <kelvin.gaia@matchboxbrasil.com>
2020-01-03 18:03:28 -0300

    Storaging at files

--- models/product.js
+++ models/product.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+const utilPath = require("../util/path");
 const products = [];
 
 module.exports = class Product {
@@ -6,10 +9,27 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    // products.push(this);
+    const p = path.join(utilPath, "data", "products.json");
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    const p = path.join(utilPath, "data", "products.json");
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        cb([]);
+      }
+      return cb(JSON.parse(fileContent));
+    });
   }
 };
