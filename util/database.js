fecbf951091d3155e1564e35a311e5173d165e3f
Kelvin Maues <kgmdeveloper@gmail.com>
2020-04-20 20:22:14 -0300

    Testing db connection with mysql2

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   database: "node-complete",
-  password: "l1ghts1d3",
+  // password: "l1ghts1d3",
 });
 
 module.exports = pool.promise();
