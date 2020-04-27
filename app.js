40e925f50a9d7c38207f58c7be294ec2e44bcd5a
Kelvin Maues <kgmdeveloper@gmail.com>
2020-04-26 21:29:38 -0300

    Adding an order model

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const Product = require("./models/product");
 const User = require("./models/user");
 const Cart = require("./models/cart");
 const CartItem = require("./models/cart-item");
+const Order = require("./models/order");
+const OrderItem = require("./models/order-item");
 // routes
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -43,6 +45,9 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
 // DB connection to sync models to tables and its relations
 sequelize
