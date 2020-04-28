338a92125b76623167b7a83f48ff938209c8f606
Kelvin Gaia Maués <kelvin.gaia@matchboxbrasil.com>
2020-04-28 13:55:36 -0300

    MongoDB setup and installed with mongodb driver

--- util/database.js
+++ util/database.js
@@ -1,8 +1,16 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize("node-complete", "root", "", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const URL_DB =
+  "mongodb+srv://konoha_ninja:0CEsTnINuRC2JS8s@boruton-kan-sbr93.mongodb.net/test?retryWrites=true&w=majority";
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+  MongoClient.connect(URL_DB)
+    .then((client) => {
+      console.log("Connected to the cluster");
+      callback(client);
+    })
+    .catch((err) => console.log(err));
+};
+
+module.exports = mongoConnect;
