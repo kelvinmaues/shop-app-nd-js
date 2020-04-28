a87094f6a793d58b547f768b2794cc31ced127ad
Kelvin Maues <kgmdeveloper@gmail.com>
2020-04-28 17:18:57 -0300

    Created method to save user to the database

--- models/product.js
+++ models/product.js
@@ -8,29 +8,13 @@ class Product {
     this.imageUrl = imageUrl;
   }
 
-  save() {}
+  save() {
+    const db = getDb();
+    db.collection("products")
+      .insertOne(this)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }
 }
-
-// const Product = sequelize.define("product", {
-//   id: {
-//     type: Sequelize.INTEGER,
-//     autoIncrement: true,
-//     allowNull: false,
-//     primaryKey: true,
-//   },
-//   title: Sequelize.STRING,
-//   price: {
-//     type: Sequelize.DOUBLE,
-//     allowNull: false,
-//   },
-//   imageUrl: {
-//     type: Sequelize.STRING,
-//     allowNull: false,
-//   },
-//   description: {
-//     type: Sequelize.STRING,
-//     allowNull: false,
-//   },
-// });
 
 module.exports = Product;
