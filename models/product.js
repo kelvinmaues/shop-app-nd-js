d7c99a95409564800791fbad744424836a460110
Kelvin Gaia Maués <kelvin.gaia@matchboxbrasil.com>
2020-05-01 15:27:39 -0300

    Storing the user in our database

--- models/product.js
+++ models/product.js
@@ -2,11 +2,12 @@ const mongodb = require("mongodb");
 const { getDb } = require("../util/database");
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, userId) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this.userId = userId;
   }
 
   // Save method to add or insert one product to this collection
