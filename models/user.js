c49801183eae306c386c04ebc9923813ffe707fb
Kelvin Maues <kgmdeveloper@gmail.com>
2020-05-01 15:40:23 -0300

    Working on cart items and orders

--- models/user.js
+++ models/user.js
@@ -2,14 +2,29 @@ const mongodb = require("mongodb");
 const { getDb } = require("../util/database");
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, userId) {
     this.name = username;
     this.email = email;
+    this.cart = cart;
+    this._id = userId;
   }
 
   save() {
     const db = getDb();
     return db.collection("users").insertOne(this);
+  }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex((cp) => {
+    //   return cp._id === product._id;
+    // });
+    product.quantity = 1;
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    return db.collection("users").updateOne(
+      { _id: new mongodb.ObjectID(this._id) },
+      { $set: { cart: updatedCart } }
+    );
   }
 
   static findById(userId) {
