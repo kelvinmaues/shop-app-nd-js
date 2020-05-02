b0a4ccd878435769412bb0f3294b99f4bcde60d4
Kelvin Gaia Maués <kelvin.gaia@matchboxbrasil.com>
2020-05-02 17:55:14 -0300

    Connecting to the database with mongoose ODM

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
 const app = express();
-// database
-const { mongoConnect } = require("./util/database");
 // models
-const User = require("./models/user");
+// const User = require("./models/user");
 // routes
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+// const adminRoutes = require("./routes/admin");
+// const shopRoutes = require("./routes/shop");
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -19,21 +18,31 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // static files
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  User.findById("5eac4e86ea397d0eba1a9cc3")
-    .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch((err) => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById("5eac4e86ea397d0eba1a9cc3")
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch((err) => console.log(err));
+// });
 
 // Set routes
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
+// app.use("/admin", adminRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3003);
-});
+mongoose
+  .connect(
+    "mongodb+srv://konoha_ninja:0CEsTnINuRC2JS8s@boruton-kan-sbr93.mongodb.net/test?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then((res) => {
+    console.log(res)
+    app.listen(3003);
+  })
+  .catch((err) => console.log(err));
