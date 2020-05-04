0fe08cd7a2f389a99b59c3532249309a349995f6
Kelvin Gaia Maués <kelvin.gaia@matchboxbrasil.com>
2020-05-03 21:11:15 -0300

    Creating a order

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,6 @@
 // models
 const Product = require("../models/product");
+const Order = require("../models/order");
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -74,14 +75,28 @@ exports.postCartDeleteProduct = (req, res, next) => {
     .catch((err) => console.log(err));
 };
 
-// exports.postOrder = (req, res, next) => {
-//   req.user
-//     .addOrder()
-//     .then(() => {
-//       res.redirect("/orders");
-//     })
-//     .catch((err) => console.log(err));
-// };
+exports.postOrder = (req, res, next) => {
+  req.user
+    .populate("cart.items.productId")
+    .execPopulate()
+    .then((user) => {
+      const products = user.cart.items.map((i) => {
+        return { quantity: i.quantity, product: i.productId };
+      });
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user,
+        },
+        products,
+      });
+      return order.save();
+    })
+    .then(() => {
+      res.redirect("/orders");
+    })
+    .catch((err) => console.log(err));
+};
 
 // exports.getOrders = (req, res, next) => {
 //   req.user
