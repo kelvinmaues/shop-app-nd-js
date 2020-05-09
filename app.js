c76ef3795d024a4a969466fbf95c3c72b2ff8ff1
Kelvin Gaia Maués <kelvin.gaia@matchboxbrasil.com>
2020-05-09 19:01:24 -0300

    Init session middleware with a new package

--- app.js
+++ app.js
@@ -2,22 +2,31 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const mongoose = require("mongoose");
+const session = require("express-session");
 
 const errorController = require("./controllers/error");
 const app = express();
+
 // models
 const User = require("./models/user");
+
 // routes
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
 
+// view engine
 app.set("view engine", "ejs");
 app.set("views", "views");
+
+// ==> MIDDLEWARES
 // parser
 app.use(bodyParser.urlencoded({ extended: false }));
 // static files
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({ secret: "my secret", resave: false, saveUninitialized: false })
+);
 
 app.use((req, res, next) => {
   User.findById("5eaeffeb57cda8298736577b")
