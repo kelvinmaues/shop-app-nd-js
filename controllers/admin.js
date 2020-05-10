39bc29ce17fb0b8ac78c9ecda9a3370cfb7a4164
Kelvin Maues <kgmdeveloper@gmail.com>
2020-05-10 17:32:50 -0300

    Using middleware to protect private user routes

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,6 @@
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  const { isLoggedIn } = req.session;
-
-  if (!isLoggedIn) {
-    return res.redirect("/login");
-  }
-
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
