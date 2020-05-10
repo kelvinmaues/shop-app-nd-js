27e152430032c6fd268727808eccac2e228855e1
Kelvin Maues <kgmdeveloper@gmail.com>
2020-05-10 13:02:55 -0300

    Fixing two tiny problems

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,3 @@
-// models
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
@@ -15,20 +14,73 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const userId = req.user;
-  const product = new Product({ title, price, description, imageUrl, userId });
+  const product = new Product({
+    title: title,
+    price: price,
+    description: description,
+    imageUrl: imageUrl,
+    userId: req.user,
+  });
   product
     .save()
-    .then(() => {
-      res.redirect("/");
+    .then((result) => {
+      // console.log(result);
+      console.log("Created Product");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect("/");
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId)
+    .then((product) => {
+      if (!product) {
+        return res.redirect("/");
+      }
+      res.render("admin/edit-product", {
+        pageTitle: "Edit Product",
+        path: "/admin/edit-product",
+        editing: editMode,
+        product: product,
+        isAuthenticated: req.session.isLoggedIn,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+
+  Product.findById(prodId)
+    .then((product) => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.description = updatedDesc;
+      product.imageUrl = updatedImageUrl;
+      return product.save();
+    })
+    .then((result) => {
+      console.log("UPDATED PRODUCT!");
+      res.redirect("/admin/products");
     })
     .catch((err) => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
   Product.find()
-    // .select("title price -_id")
-    // .populate("userId", "name")
+    // .select('title price -_id')
+    // .populate('userId', 'name')
     .then((products) => {
       console.log(products);
       res.render("admin/products", {
@@ -41,47 +93,11 @@ exports.getProducts = (req, res, next) => {
     .catch((err) => console.log(err));
 };
 
-exports.getEditProduct = (req, res, next) => {
-  const { edit } = req.query;
-  const { productId } = req.params;
-
-  if (!edit) {
-    return res.redirect("/");
-  }
-  Product.findById(productId)
-    .then((product) => {
-      if (!product) {
-        return res.redirect("/");
-      }
-      res.render("admin/edit-product", {
-        pageTitle: "Edit Product",
-        path: "/admin/edit-product",
-        editing: edit,
-        product,
-        isAuthenticated: req.session.isLoggedIn,
-      });
-    })
-    .catch((err) => console.log(err));
-};
-
-exports.postEditProduct = (req, res, next) => {
-  const { productId, title, price, imageUrl, description } = req.body;
-  Product.findById(productId)
-    .then((doc) => {
-      doc.title = title;
-      doc.price = price;
-      doc.imageUrl = imageUrl;
-      doc.description = description;
-      return doc.save();
-    })
-    .then(() => res.redirect("/admin/products"))
-    .catch((err) => console.log(err));
-};
-
 exports.postDeleteProduct = (req, res, next) => {
-  const { productId } = req.body;
-  Product.findByIdAndRemove(productId)
+  const prodId = req.body.productId;
+  Product.findByIdAndRemove(prodId)
     .then(() => {
+      console.log("DESTROYED PRODUCT");
       res.redirect("/admin/products");
     })
     .catch((err) => console.log(err));
