4315878e55bafa1f0793c2a09833aae560351527
Kelvin Maues <kgmdeveloper@gmail.com>
2020-05-24 13:46:54 -0300

    Keeping user input in sign up after a request

--- controllers/auth.js
+++ controllers/auth.js
@@ -30,6 +30,7 @@ exports.getSignup = (req, res, next) => {
     path: "/signup",
     pageTitle: "Signup",
     errorMessage: errMsg.length > 0 ? errMsg : null,
+    oldInput: { name: "", email: "", password: "", confirmPassword: "" },
   });
 };
 
@@ -72,7 +73,7 @@ exports.postLogin = (req, res, next) => {
 };
 
 exports.postSignup = (req, res, next) => {
-  const { name, email, password } = req.body;
+  const { name, email, password, confirmPassword } = req.body;
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log(errors.array());
@@ -80,6 +81,7 @@ exports.postSignup = (req, res, next) => {
       path: "/signup",
       pageTitle: "Signup",
       errorMessage: errors.array()[0].msg,
+      oldInput: { name, email, password, confirmPassword },
     });
   }
 
