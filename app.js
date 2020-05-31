853c32ec6612d7ed3fb5be087d0a22f49db61d5f
Kelvin Gaia Maués <kelvin.gaia@matchboxbrasil.com>
2020-05-31 15:13:54 -0300

    Using the error handling middleware correctly

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.use((req, res, next) => {
       next();
     })
     .catch((err) => {
-      throw new Error(err);
+      next(new Error(err));
     });
 });
 
@@ -79,7 +79,11 @@ app.get("/500", errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-  res.redirect("/500");
+  res.status(500).render("500", {
+    pageTitle: "Error!",
+    path: "/500",
+    isAuthenticated: req.session.isLoggedIn,
+  });
 });
 
 mongoose
