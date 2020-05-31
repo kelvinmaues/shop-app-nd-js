cc84a4f6f111c4fac9c9770c88e025570b414b17
Kelvin Gaia Maués <kelvin.gaia@matchboxbrasil.com>
2020-05-31 13:48:10 -0300

    Returning errors page

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,5 @@
+const mongoose = require("mongoose");
+
 const { validationResult } = require("express-validator");
 const Product = require("../models/product");
 
@@ -22,7 +24,7 @@ exports.postAddProduct = (req, res, next) => {
   if (!errors.isEmpty()) {
     return res.status(422).render("admin/edit-product", {
       pageTitle: "Add Product",
-      path: "/admin/edit-product",
+      path: "/admin/add-product",
       editing: false,
       hasError: true,
       product: {
@@ -35,7 +37,9 @@ exports.postAddProduct = (req, res, next) => {
       validationErrors: errors.array(),
     });
   }
+
   const product = new Product({
+    _id: new mongoose.Types.ObjectId("5ed2b5fffd75af277b125db0"),
     title: title,
     price: price,
     description: description,
@@ -48,7 +52,21 @@ exports.postAddProduct = (req, res, next) => {
       res.redirect("/admin/products");
     })
     .catch((err) => {
-      console.log(err);
+      // return res.status(500).render("admin/edit-product", {
+      //   pageTitle: "Add Product",
+      //   path: "/admin/add-product",
+      //   editing: false,
+      //   hasError: true,
+      //   product: {
+      //     title,
+      //     imageUrl,
+      //     price,
+      //     description,
+      //   },
+      //   errorMessage: "Database operation failed, please try again.",
+      //   validationErrors: [],
+      // });
+      res.redirect("/500");
     });
 };
 
