7c2f5f5c397c5113e170f7b1e09671217b8c4364
Kelvin Gaia Maués <kelvin.gaia@matchboxbrasil.com>
2020-06-16 18:51:06 -0300

    Manipulating the DOM

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,6 +1,7 @@
 const deleteProduct = (btn) => {
   const prodId = btn.parentNode.querySelector("[name=productId]").value;
   const csrf = btn.parentNode.querySelector("[name=_csrf]").value;
+  const productElement = btn.closest("article");
 
   fetch(`/admin/product/${prodId}`, {
     method: "DELETE",
@@ -10,6 +11,11 @@ const deleteProduct = (btn) => {
   })
     .then((res) => {
       console.log(res);
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      productElement.parentNode.removeChild(productElement);
     })
     .catch((err) => {
       console.log(err);
